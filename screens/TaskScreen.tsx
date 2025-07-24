ea31f5781a9cdd2f5cd621951ef69726a8aad852
WesleyWeb30 <WesleyWeb3.0@gmail.com>
2025-07-24 03:02:27 -0300

    chore: pass handleEdit to TaskList and update function names

--- screens/TaskScreen.tsx
+++ screens/TaskScreen.tsx
@@ -4,20 +4,26 @@ import { useTasks } from '../src/hooks/useTasks';
 import TaskList from '../src/components/TaskList';
 import { TaskForm } from '../src/components/TaskForm';
 
-
 export default function TaskScreen() {
-  const { tasks, titulo, setTitulo, loading, error, action: {handleDone, handleCreate, handleDelete} } = useTasks();
+  const { tasks, titulo, setTitulo, loading, error, action: { handleToggleDone, handleCreate, handleDelete, handleEdit } } = useTasks();
 
   return (
     <SafeAreaView style={styles.container}>
       <TaskForm titulo={titulo} setTitulo={setTitulo} onCreate={handleCreate} />
       <Text style={styles.title}>Lista de Tarefas</Text>
-      <TaskList tasks={tasks} loading={loading} error={error} onDone={handleDone}  onDelete={handleDelete}/>
+      <TaskList 
+        tasks={tasks} 
+        loading={loading} 
+        error={error} 
+        onDone={handleToggleDone} 
+        onDelete={handleDelete} 
+        onEdit={handleEdit} 
+      />
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  container: { flex: 1, backgroundColor: '#FFFFFF' },
-  title: { fontSize: 24, fontWeight: 'bold', textAlign: 'center', marginVertical: 20 },
+  container: { flex: 1, backgroundColor: '#f9f9f9' },
+  title: { fontSize: 24, fontWeight: 'bold', textAlign: 'center', marginVertical: 20, color: '#333' },
 });
